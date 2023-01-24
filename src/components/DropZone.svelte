5a13ce07cf5fd103b0e4e9f3b5ece980c6d07d28
kinchiller <kingchiller@gmail.com>
2023-01-24 21:36:18 +0100

    Add some fun

--- src/components/DropZone.svelte
+++ src/components/DropZone.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import MdFileUpload from 'svelte-icons/md/MdFileUpload.svelte'
+  import gif from '$lib/images/snoop.webp'
 
   type OnDrop = (files: File[]) => void
   export let onDrop: OnDrop
@@ -28,6 +29,7 @@
     }
   }
   const handleDrop = (e: DragEvent) => {
+    e.preventDefault()
     if (disabled) {
       return
     }
@@ -42,21 +44,26 @@
 
   const handleDragOver = (e: DragEvent) => {
     e.preventDefault()
+    if (!isOver) {
+      isOver = true
+    }
   }
 
   const handleChange = (e: Event) => {
-    let files: FileList = <FileList>(<HTMLInputElement>e.target).files
     e.preventDefault()
+    let files: FileList = <FileList>(<HTMLInputElement>e.target).files
     onDrop(Array.from(files))
   }
 </script>
 
+<svelte:body
+  on:dragenter={handleEnter}
+  on:dragover={handleDragOver}
+  on:drop={handleDrop}
+  on:dragleave={handleLeave}
+/>
 <div
   class="relative border-2 text-pink-500 bg-white border-dashed rounded-lg border-pink-500 hover:border-pink-600 focus-within:border-blue-500 focus-within:shadow-lg hover:shadow-lg hover:shadow-indigo-200/50 transition"
-  on:drop={handleDrop}
-  on:dragover={handleDragOver}
-  on:dragenter={handleEnter}
-  on:dragleave={handleLeave}
 >
   <slot>
     <div class="p-4 flex flex-col items-center justify-center">
@@ -69,10 +76,15 @@
 
   <div
     id="overlay"
-    class="absolute top-0 left-0 w-full h-full pointer-events-none rounded-lg flex items-center justify-center bg-white opacity-0"
+    class="fixed top-0 left-0 w-full h-full pointer-events-none rounded-lg flex items-end justify-center bg-black opacity-0"
     style={isOver ? 'opacity: 1;' : ''}
   >
-    <slot name="overlay">Drop here</slot>
+    <img class="absolute object-cover w-full h-full" alt="Snoop Dogg" src={gif} />
+    <div
+      class="w-full text-white text-center absolute text-6xl md:text-8xl p-12 pb-16 uppercase font-bold bg-gradient-to-b from-transparent to-black"
+    >
+      Drop it like it's hot
+    </div>
   </div>
   <input
     class="opacity-0 absolute top-0 left-0 w-full h-full cursor-pointer"
