c30262914b7853ba5adf661cbc9d2a026fec0e8a
kinchiller <kingchiller@gmail.com>
2023-01-29 15:04:49 +0100

    Add Alert component

---
+++ src/components/Alert.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import MdErrorOutline from 'svelte-icons/md/MdErrorOutline.svelte'
+
+  let className: string = ''
+  export { className as class }
+</script>
+
+<div
+  class="{className} flex items-center p-4 leading-normal text-indigo-600 bg-indigo-50 border border-indigo-500 rounded-lg"
+  role="alert"
+>
+  <div class="inline w-6 h-6 mr-2">
+    <MdErrorOutline class="fill-current" />
+  </div>
+  <slot />
+</div>
