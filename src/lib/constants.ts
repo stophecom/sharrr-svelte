ec0322611838fa0179b350d68f005332f75374e6
kinchiller <kingchiller@gmail.com>
2023-01-30 11:54:56 +0100

    Max file size based on environment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,5 +3,5 @@ import { PUBLIC_ENV } from '$env/static/public'
 export const MB = 10 ** 6 // 1000000 Bytes = 1 MB.
 export const GB = 10 ** 9 // 1000000000 Bytes = 1 GB.
 
-export const MAX_FILE_SIZE = 100 * GB
+export const MAX_FILE_SIZE = (PUBLIC_ENV === 'production' ? 100 : 1) * GB
 export const CHUNK_SIZE = (PUBLIC_ENV === 'production' ? 100 : 1) * MB
