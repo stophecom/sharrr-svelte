b2c2f94af13e3bb13c10662e7c2d60a2be200dda
kinchiller <kingchiller@gmail.com>
2023-01-30 11:36:56 +0100

    Set chunk size based on environment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,7 @@
+import { VERCEL_ENV } from '$env/static/private'
+
 export const MB = 10 ** 6 // 1000000 Bytes = 1 MB.
 export const GB = 10 ** 9 // 1000000000 Bytes = 1 GB.
 
 export const MAX_FILE_SIZE = 100 * GB
-export const CHUNK_SIZE = 1 * MB // @todo Increase when production ready
+export const CHUNK_SIZE = (VERCEL_ENV === 'production' ? 100 : 1) * MB // @todo Increase when production ready
