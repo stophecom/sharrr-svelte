6ef9c9bcb0f7e0a379dd1c922300023a457a3f13
kinchiller <kingchiller@gmail.com>
2023-01-31 21:22:36 +0100

    Fix production URL for sharing link

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,6 @@
+export const productionDomain = 'https://sharrr.com'
 export const MB = 10 ** 6 // 1000000 Bytes = 1 MB.
 export const GB = 10 ** 9 // 1000000000 Bytes = 1 GB.
 
-export const getMaxFileSize = (env) => (env === 'production' ? 100 : 1) * GB
-export const getChunkSize = (env) => (env === 'production' ? 100 : 1) * MB
+export const getMaxFileSize = (env: string) => (env === 'production' ? 100 : 1) * GB
+export const getChunkSize = (env: string) => (env === 'production' ? 100 : 1) * MB
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,10 +1,12 @@
 import { VERCEL_URL } from '$env/static/private'
+import { PUBLIC_ENV } from '$env/static/public'
 import type { PageServerLoad } from './$types'
+import { productionDomain } from '$lib/constants'
 
 const sanitizeUrl = (url: string) => (url.startsWith('http') ? url : `https://${url}`)
 
 export const load = (() => {
   return {
-    vercelUrl: sanitizeUrl(VERCEL_URL)
+    vercelUrl: PUBLIC_ENV === 'production' ? productionDomain : sanitizeUrl(VERCEL_URL)
   }
 }) satisfies PageServerLoad
