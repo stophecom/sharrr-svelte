3ba96c1a0ac676b6e5df5067edf66ce94d19e778
kinchiller <kingchiller@gmail.com>
2023-02-01 21:31:42 +0100

    Basic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import Error from '$components/Error.svelte'
+  import Page from '$components/Page.svelte'
+  import { page } from '$app/stores'
+</script>
+
+<svelte:head>
+  <title>Sharrr: Something went wrong.</title>
+</svelte:head>
+
+<Page title={'Ooops. '} subtitle={'Something went wrong.'}>
+  <div class="flex justify-center">
+    <Error>
+      {$page?.error?.message}
+    </Error>
+  </div>
+</Page>
