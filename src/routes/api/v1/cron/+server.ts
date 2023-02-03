c2bb64962081e2d50b9570b1ea33199d2cc50e77
kinchiller <kingchiller@gmail.com>
2023-02-03 09:51:45 +0100

    Delete old secrets with cron job.

--- src/routes/api/v1/cron/+server.ts
+++ src/routes/api/v1/cron/+server.ts
@@ -6,9 +6,15 @@ import { API_SECRET_KEY } from '$env/static/private'
 import { PUBLIC_FLOW_S3_BUCKET } from '$env/static/public'
 import { getS3Client } from '$lib/s3'
 import { fileRetentionPeriodInDays } from '$lib/constants'
+import prisma from '$lib/prisma'
 
 const BucketName = PUBLIC_FLOW_S3_BUCKET
 const client = getS3Client()
+
+function subtractDays(date: Date, days: number) {
+  date.setDate(date.getDate() - days)
+  return date
+}
 
 type ObjectList = { Key: string }[]
 export const POST: RequestHandler = async ({ request }) => {
@@ -16,8 +22,7 @@ export const POST: RequestHandler = async ({ request }) => {
 
   if (authorization === `Bearer ${API_SECRET_KEY}`) {
     // Delete files older than X days
-    const deleteFilesBeforeDate = new Date()
-    deleteFilesBeforeDate.setDate(deleteFilesBeforeDate.getDate() - fileRetentionPeriodInDays) // X days in the past
+    const deleteFilesBeforeDate = subtractDays(new Date(), fileRetentionPeriodInDays)
 
     for await (const data of paginateListObjectsV2(
       { client, pageSize: 1000 },
@@ -37,14 +42,33 @@ export const POST: RequestHandler = async ({ request }) => {
         }
       }).filter(Boolean) as ObjectList
 
-      if (!s3ObjectsToDelete.length) {
-        throw error(500, 'No files to delete')
+      if (s3ObjectsToDelete.length) {
+        const bucketParams = { Bucket: BucketName, Delete: { Objects: s3ObjectsToDelete } }
+        await client.send(new DeleteObjectsCommand(bucketParams))
+        console.log(`Cron: Deleted ${s3ObjectsToDelete.length} files from S3.`)
+      } else {
+        console.log(`Cron: No files to delete from S3.`)
       }
-
-      const bucketParams = { Bucket: BucketName, Delete: { Objects: s3ObjectsToDelete } }
-      await client.send(new DeleteObjectsCommand(bucketParams))
     }
 
+    // Delete secrets that have been retrieved older than 1 days
+    const deleteRetrievedSecretsBeforeDate = subtractDays(new Date(), 1)
+    // Delete secrets that have not been retrieved older than 7 days
+    const deleteCreatedSecretsBeforeDate = subtractDays(new Date(), 7)
+    const { count } = await prisma.secret.deleteMany({
+      where: {
+        OR: [
+          {
+            retrievedAt: { lte: deleteRetrievedSecretsBeforeDate }
+          },
+          {
+            createdAt: { lte: deleteCreatedSecretsBeforeDate }
+          }
+        ]
+      }
+    })
+
+    console.log(`Cron: Deleted ${count} entries from the Secrets database.`)
     return json({ success: true })
   } else {
     throw error(401, 'Unauthorized')
