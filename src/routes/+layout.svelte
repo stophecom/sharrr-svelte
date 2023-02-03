a7ce0dc178fcb0fb4b3bae0d4bdce78af577b7f5
kinchiller <kingchiller@gmail.com>
2023-02-03 18:06:05 +0100

    Update favicon images

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,6 +15,12 @@
     name="keywords"
     content="pirates, share file, end-to-end encrypted file sharing, zero knowledge encrypted file transfer, pirates sharrr, arrr"
   />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+  <link rel="manifest" href="/site.webmanifest" />
+  <meta name="msapplication-TileColor" content="#da532c" />
+  <meta name="theme-color" content="#ffffff" />
 </svelte:head>
 
 <div class="app">
