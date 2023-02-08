673921124b9e0c33d3394f873d783e2dd92657cc
kinchiller <kingchiller@gmail.com>
2023-02-08 14:09:47 +0100

    Add x-robot header

---
+++ tests/hooks.server.ts
@@ -0,0 +1,11 @@
+import type { Handle } from '@sveltejs/kit'
+import { PUBLIC_ENV } from '$env/static/public'
+
+export const handle = (async ({ event, resolve }) => {
+  const response = await resolve(event)
+
+  if (PUBLIC_ENV !== 'production') {
+    response.headers.set('X-Robots-Tag', 'noindex')
+  }
+  return response
+}) satisfies Handle
