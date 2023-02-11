a81cef30e881319cffc70a83baa056592307c1f7
kinchiller <kingchiller@gmail.com>
2023-02-11 09:24:24 +0100

    Improve layout

--- src/components/Page.svelte
+++ src/components/Page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <section class="flex items-center">
-  <div class="relative pt-20 sm:pt-28 pb-12">
+  <div class="relative py-12">
     <div class="container mx-auto px-6 lg:px-8">
       <h1
         class="mb-5 leading-tight text-5xl sm:text-5xl md:text-6xl font-bold tracking-tight sm:text-center "
